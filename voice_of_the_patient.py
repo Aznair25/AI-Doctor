28d3627ef4c00dc1f70be391164d6492e75843e1
Aznair Manzoor <aznairmanzoor1@gmail.com>
2025-04-05 02:46:18 -0700

    voice of the patient and transcription

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,1 +1,54 @@
 #step 1:set up audio recorder(ffmped and portaudio)
+import logging
+import speech_recognition as sr
+from pydub import AudioSegment
+from io import BytesIO
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+
+def record_audio(file_path,timeout=20,pharase_time_limit=None):
+    """
+    SImplified function to record audio from microphone and save it as MP3 file.
+
+    Args:
+    file_path (str): Path to save recorded audio file.
+    timeout (int): Maximum time to wait for a phrase to start (in seconds).
+    pharase_time_limit (int): Maximum time for the pharase to be recoded (in seconds).
+    """
+    recognizer=sr.Recognizer()
+    try:
+        with sr.Microphone() as source:
+            logging.info("Adjusting for Ambient Noice.....")
+            recognizer.adjust_for_ambient_noise(source, duration=1)
+            logging.info("Start speaking Now.....")
+
+            #record audio
+            audio_data=recognizer.listen(source,timeout=timeout,phrase_time_limit=pharase_time_limit)
+            logging.info("Recording Complete......")
+            #convert the recorded audio to mp3 file
+            wav_data=audio_data.get_wav_data()
+            audio_segment=AudioSegment.from_wav(BytesIO(wav_data))
+            audio_segment.export(file_path,format="mp3",bitrate='128k')
+
+            logging.info(f"Audio saved to {file_path}")
+
+    except Exception as e:
+        logging.error(f"An error occured: {e}")
+audio_file_path="voice_of_the_patient.mp3"
+record_audio(file_path=audio_file_path)    
+
+#step 2: Set up speech to text-SST-model for transcription
+import os 
+from groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+client=Groq(api_key=GROQ_API_KEY)
+stt_model="whisper-large-v3"
+audio_file=open(audio_file_path,"rb")
+transcription=client.audio.transcriptions.create(
+    model=stt_model,
+    file=audio_file,
+    language="en"
+)
+print(transcription.text)
